33008b83c0c4212a1086dac76379482064559a7a
zwy <zwy_qz@hotmail.com>
2014-03-27 10:01:58 +0800

    测试validate

--- WebRoot/validate_form.jsp
+++ WebRoot/validate_form.jsp
@@ -7,8 +7,10 @@
   </head>  
   <body>
     <s:form action="new_validate" namespace="/test" >
-        <s:textfield name="name" label="姓名"  />
-        <s:textfield name="age" label="年龄"/> 
+        <s:textfield name="msg" label="姓名" />
+<s:textfield name="age" label="年龄"/>
+<s:textfield name="user.name" label="姓名1" />
+<s:textfield name="user.age" label="年龄1"/>
         <s:submit/>
     </s:form> 
   </body>  
--- src/action/NewValidateAction.java
+++ src/action/NewValidateAction.java
@@ -2,15 +2,25 @@ package action;
 
 import com.opensymphony.xwork2.ActionSupport;
 
+import data.User;
+
 public class NewValidateAction extends ActionSupport
 {
     private String name;  // 必须输入
     private int age;   // 在13和20之间
+    private User user;
+    
     public String getName()
     {
         return name;
     }
-    public void setName(String name)
+    public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public void setName(String name)
     {
     	System.out.println("msg is :"+name);
         this.name = name;
